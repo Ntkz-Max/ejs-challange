a6b477602c470599d0aa555fa2463f7bacc0bcdf
Ntkz-Max <85770292+Ntkz-Max@users.noreply.github.com>
2023-05-11 23:20:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,22 +16,61 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+let posts = [];
 
 app.get("/", function(req,res){
+  res.render("home",{
+    homeContent:homeStartingContent,
+    posts:posts
+  });
+});
 
-  res.render("home");
+app.get("/about", function(req,res){
+  res.render("about",{aboutUsContent:aboutContent});
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact",{contactUsContent:contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req,res){
+
+const post =  {
+  title :  req.body.postTitle,
+  body:  req.body.postBody
+};
+
+posts.push(post);
+res.redirect("/");
+
 });
 
 
 
 
+app.get("/post/:content", function(req,res){
+const requestedPage = _.lowerCase(req.params.content);
 
+ posts.forEach(function(post){
+   var title = _.lowerCase(post.title);
 
+  if(title === requestedPage){
+    res.render("post",{
+      pageTitle:post.title,
+      pageContent:post.body
+    });
+  }
 
+ });
+
+});
 
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server running smoothly on port 3000");
 });
